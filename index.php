9718476837d48443db801239f388d0e6269194b0
MaybeMercy <2460572865@qq.com>
2015-04-13 22:18:49 +0800

    change but not work, need soap lib

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+  header("Content-type:text/html;charset=utf-8");
   $base_url = "http://www.webxml.com.cn/WebServices/WeatherWebService.asmx/getWeatherbyCityName";
   # has the data
   $hasName = false;
@@ -18,14 +19,30 @@
         ?>>
         <input type="submit" value="submit">
     </form>
+    <div>
+        <?php
+        /**
+         * Created by PhpStorm.
+         * User: Maybe霏
+         * Date: 2015/4/13
+         * Time: 19:42
+         */
+        if($hasName){
+            $fields=array(
+                'theCityName'=>$_REQUEST['theCityName']
+            );
+            $ch=curl_init();
+            curl_setopt($ch,CURLOPT_URL,$base_url);
+            curl_setopt($ch,CURLOPT_POST,count($fields));
+            curl_setopt($ch,CURLOPT_POSTFIELDS,$fields);
+            ob_start();
+            curl_exec($ch);
+            $result=ob_get_contents();
+            ob_end_clean();
+            echo $result;
+            curl_close($ch);
+        }
+        ?>
+    </div>
 </body>
 </html>
-<?php
-/**
- * Created by PhpStorm.
- * User: Maybe霏
- * Date: 2015/4/13
- * Time: 19:42
- */
-
-?>
